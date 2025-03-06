587b671fdaf05160b7845cba001f51bc94e9ae5f
Santiago Alarcón <santialfa67@gmail.com>
2025-03-05 22:19:43 -0500

    CuentaBancaria

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,52 +2,111 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        boolean jugar = true;
+        int opcion = 0;
 
-        while (jugar) {
-            RuletaRusa juego = new RuletaRusa();
-            List<String> jugadores = new ArrayList<>();
+        List<CuentaBancaria> cuentas = new ArrayList<>();
 
-            System.out.println(".....::::::::RULETA RUSA::::::........");
-            System.out.print("Ingrese el numero de jugadores: ");
-            int numJugadores = teclado.nextInt();
+        CuentaBancaria cuenta1 = new CuentaBancaria("Santiago", "1000000", 100.0);
+        CuentaBancaria cuenta2 = new CuentaBancaria("Papayo", "110000",  10.0);
+        CuentaBancaria cuenta3 =new CuentaBancaria();
+
+        cuentas.add(cuenta1);
+        cuentas.add(cuenta2);
+        cuentas.add(cuenta3);
+
+        System.out.println(cuenta1.toString());
+        System.out.println(cuenta2.toString());
+
+
+        cuenta3.setTitular("Aurelio Cheveroni");
+        cuenta3.setNumeroCuenta("1110000");
+        cuenta3.setSaldo(50.0);
+
+        System.out.println(cuenta3);
+
+        do {
+            System.out.println("1. Ver cuentas disponibles");
+            System.out.println("2. Acceder a una cuenta");
+            System.out.println("3. Crear una cuenta");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opcion: ");
+            opcion = teclado.nextInt();
             teclado.nextLine();
 
-            for (int i = 1; i <= numJugadores; i++) {
-                System.out.print("Ingrese el nombre del jugador " + i + ": ");
-                jugadores.add(teclado.nextLine());
-            }
-
-            System.out.println("La bala está en la posicion: " + juego.bala);
-            int turno = new Random().nextInt(numJugadores);
-            System.out.println("El jugador que empieza es: " + jugadores.get(turno));
-
-            boolean rondaActiva = true;
-
-            while (rondaActiva) {
-                String jugadorActual = jugadores.get(turno);
-                System.out.println("Turno de " + jugadorActual);
-                System.out.println("Presiona enter para disparar");
-                String input = teclado.nextLine();
-
-
-                if (juego.disparar()) {
-                    System.out.println("....:::::::SONIDO DE DISPARO:::::......");
-                    System.out.println("El jugador " + jugadorActual + " MURIO");
-                    rondaActiva = false;
-                } else {
-                    System.out.println("......::::::CLACK CLACK:::::.......");
-                    System.out.println("El jugador " + jugadorActual + " Se salvo");
-                    turno = (turno + 1) % numJugadores;
+            switch (opcion) {
+                case 1 -> {
+                    System.out.println("Cuentas Disponibles");
+                    for (CuentaBancaria cuenta : cuentas) {
+                        System.out.println("Titular: " + cuenta.getTitular() + " Numero de Cuenta: " + cuenta.getNumeroCuenta() +
+                                " Saldo: " + cuenta.getSaldo());
+                    }
                 }
+                case 2 -> {
+                    System.out.print("Ingrese su numero de cuenta: ");
+                    String numeroIngresado = teclado.nextLine();
+
+                    CuentaBancaria cuentaSeleccionada = buscarCuentaPorNumero(cuentas, numeroIngresado);
+                    if (cuentaSeleccionada != null) {
+                        System.out.println("Acceso permitido " + cuentaSeleccionada.getTitular());
+                        int opcionCuenta;
+                        do {
+                            System.out.println("Operaciones para " + cuentaSeleccionada.getTitular() + " ");
+                            System.out.println("1. Depositar dinero");
+                            System.out.println("2. Retirar dinero");
+                            System.out.println("3. Menu principal");
+                            System.out.print("Seleccione una opcion: ");
+                            opcionCuenta = teclado.nextInt();
+
+                            switch (opcionCuenta) {
+                                case 1 -> {
+                                    System.out.print("Ingrese cantidad: ");
+                                    double cantidad = teclado.nextDouble();
+                                    cuentaSeleccionada.depositarDinero(cantidad);
+                                }
+                                case 2 -> {
+                                    System.out.print("Ingrese cantidad: ");
+                                    double cantidad = teclado.nextDouble();
+                                    cuentaSeleccionada.retirarDinero(cantidad);
+                                }
+                            }
+                        } while (opcionCuenta != 3);
+                    } else {
+                        System.out.println("Numero de cuenta invalido.");
+                    }
+                }
+                case 3 -> {
+                    System.out.print("Ingrese el nombre del titular: ");
+                    String titular = teclado.nextLine();
+
+                    System.out.print("Ingrese el número de cuenta: ");
+                    String numeroCuenta = teclado.nextLine();
+
+                    System.out.print("Ingrese el saldo inicial: ");
+                    double saldo = teclado.nextDouble();
+
+                    CuentaBancaria nuevaCuenta = new CuentaBancaria(titular, numeroCuenta, saldo);
+                    cuentas.add(nuevaCuenta);
+                    System.out.println("Cuenta creada con exito.");
+                }
+                case 4 -> System.out.println("Saliendo del sistema...");
+
+                default -> System.out.println("Opcion invalida.");
+            }
+        } while (opcion != 4);
+
+    }
+
+    private static CuentaBancaria buscarCuentaPorNumero(List<CuentaBancaria> cuentas, String numeroCuenta) {
+        for (CuentaBancaria cuenta : cuentas) {
+            if (cuenta.getNumeroCuenta().equals(numeroCuenta)) {
+                return cuenta;
             }
         }
-        teclado.close();
+        return null;
     }
 }
